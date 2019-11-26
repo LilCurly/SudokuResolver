80899b893e5bdbe7b105a6a5f44625c73554823b
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-26 14:49:26 +0100

    Returning true when at least one value has been fixed

--- src/be/technifutur/devmob/solver/FixLastPossibilitySolver.java
+++ src/be/technifutur/devmob/solver/FixLastPossibilitySolver.java
@@ -7,13 +7,15 @@ public class FixLastPossibilitySolver implements SudokuSolver {
 
     @Override
     public boolean resolve(SudokuWrapper sudokuWrapper) throws SolverExcpetion {
+        boolean result = false;
         Collection<SudokuWrapper.CelluleWrapper> cells = sudokuWrapper.getCelluleCollection();
         Set<Character> possibilites;
         for(SudokuWrapper.CelluleWrapper c : cells) {
             possibilites = c.getPossibilites();
-            if(c.isEmpty() && possibilites.size() == 1) {
+            if (c.isEmpty() && possibilites.size() == 1) {
                 char value = possibilites.stream().findFirst().get();
                 sudokuWrapper.setValue(c, value);
+                result = true;
             }
         }
         return false;
