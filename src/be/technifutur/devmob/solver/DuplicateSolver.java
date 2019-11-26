92c37a226c2cf3bc321a4829456530d045b56b18
student5308 <12068940+LilCurly@users.noreply.github.com>
2019-11-26 14:47:05 +0100

    returning true if at least one iteration of the loop returns true

--- src/be/technifutur/devmob/solver/DuplicateSolver.java
+++ src/be/technifutur/devmob/solver/DuplicateSolver.java
@@ -7,20 +7,23 @@ import java.util.stream.Collectors;
 public class DuplicateSolver implements SudokuSolver {
     @Override
     public boolean resolve(SudokuWrapper sudokuWrapper) throws SolverExcpetion {
+        boolean result = false;
         for(Collection<SudokuWrapper.CelluleWrapper> values : sudokuWrapper.getLineMap().values()) {
-            handleLoop(values, sudokuWrapper);
+            result |= handleLoop(values, sudokuWrapper);
         }
         for(Collection<SudokuWrapper.CelluleWrapper> values : sudokuWrapper.getColMap().values()) {
-            handleLoop(values, sudokuWrapper);
+            result |= handleLoop(values, sudokuWrapper);
         }
         for(Collection<SudokuWrapper.CelluleWrapper> values : sudokuWrapper.getSectorMap().values()) {
-            handleLoop(values, sudokuWrapper);
+            result |= handleLoop(values, sudokuWrapper);
         }
-        return true;
+        return result;
     }
 
-    private void handleLoop(Collection<SudokuWrapper.CelluleWrapper> values, SudokuWrapper sudokuWrapper) {
+    private boolean handleLoop(Collection<SudokuWrapper.CelluleWrapper> values, SudokuWrapper sudokuWrapper) {
+        boolean resolved = false;
         Collection<Character> setValues = values.stream().filter(c -> !c.isEmpty()).map(SudokuWrapper.CelluleWrapper::getValue).collect(Collectors.toCollection(HashSet::new));
+        if(setValues.size() > 0 && setValues.size() < values.size()) resolved = true;
         values.stream().filter(SudokuWrapper.CelluleWrapper::isEmpty).forEach(c -> {
             try {
                 sudokuWrapper.removePossibilities(c, setValues);
@@ -28,5 +31,6 @@ public class DuplicateSolver implements SudokuSolver {
                 System.out.println("Cannot be removed");
             }
         });
+        return resolved;
     }
 }
